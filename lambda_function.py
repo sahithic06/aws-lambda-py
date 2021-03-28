a60dcb2968e68551c29a672797bc466dd6222910
sahuppal <sahuppal@cisco.com>
2021-03-28 01:01:12 -0400

    build lambda

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import requests
+import json
+def lambda_handler(event, context):
+    print("Event is : ")
+    print(event)
+    records = event['Records']
+    for record in records:
+        message = record['body']
+        body_without_new_line = message.replace('\\n','')
+        body = json.loads(body_without_new_line)
+        print(body['Message'])
+        response = requests.post('http://sahuppal-aws.com/persist', json=json.loads(body['Message']))
+        print(response)
+    print("I am updated from github actions")
+    return json.loads(body['Message'])
